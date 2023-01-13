c2fe2ce7512d341da0593e66186559bc72a66190
Samurai-way <olegmoishevych@gmail.com>
2023-01-13 09:37:55 +0200

    create: ArticlePage

--- src/components/article/ArticlePage.tsx
+++ src/components/article/ArticlePage.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from "../../redux/store";
-import {Box, Button, Paper} from "@mui/material";
-import img from './../../assets/Arrow - Right.png'
+import {Box, Button, Paper, Typography} from "@mui/material";
+import img from '../../assets/Arrow - Left.png';
 import {useNavigate, useParams} from "react-router-dom";
 import {getCardTC} from "../../redux/cardsReducer";
 
@@ -10,7 +10,7 @@ export const ArticlePage = () => {
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
     const card = useAppSelector(state => state.cards.card)
-
+    console.log(card)
     useEffect(() => {
         id && dispatch(getCardTC(id))
     }, [])
@@ -19,12 +19,20 @@ export const ArticlePage = () => {
     }
     return (
 
-        <Box sx={{width: '1440px'}}>
-
-            <Paper sx={{width: '1290px', height: '1001px', margin: '0 auto'}}>
-                <img style={{width: '200px', height: '100px'}} src={card.imageUrl}/>
+        <Box sx={{width: '1440px', marginLeft: '50px'}}>
+            <Paper sx={{width: '1440px', margin: '0 auto'}}>
+                <img style={{width: '1440px', height: '245px'}} src={card.imageUrl}/>
+                <Paper sx={{margin: '0px auto'}}>
+                    <Typography sx={{margin: '35px auto', display: 'flex', justifyContent: 'center'}}>
+                        {card.title}
+                    </Typography>
+                    <Box sx={{width: '1140px', margin: '0 auto'}}>
+                        <Typography sx={{margin: '50px auto', display: 'flex', justifyContent: 'center'}}>
+                            {card.summary}
+                        </Typography>
+                    </Box>
+                </Paper>
             </Paper>
-
             <Button sx={{color: 'black'}} onClick={onClickBackHandler}>
                 Back to homepage
                 <img style={{marginLeft: '5px'}} src={img}/>
